bf2d1857846dd41d41257dacd4cc6e1383997362
Diego Felix <dt196397@gmail.com>
2022-02-04 17:07:44 -0600

    Finished Js. Structure and finished project.

--- game.js
+++ game.js
@@ -7,14 +7,18 @@ Keep Score
 First score to 5 wins
 interact with user on inteface
 */
+let playerScore = 0;
+let computerScore = 0;
 const interact = document.querySelector('.interface')
 const buttons = document.querySelectorAll('button')
 buttons.forEach(element =>{
     element.addEventListener('click',e=>{
-        console.log(e.target.value)
+        console.log(e.target.value);
+        playerInteract(playRound(e.target.value));
+        checkScore(playerScore, computerScore);
     })
 })
-   
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -28,26 +32,39 @@ function computerPlay(num){
     }
     return "scissors";
 }
-function playRound(playerValue){
-let computerValue = computerPlay(getRandomInt(3));
-if (computerValue===playerValue){
-    return 'draw'
-}else if (computerValue==='rock'&&playerValue==='scissors'||computerValue==='paper'&&playerValue==='rock'||computerValue==='scissors'&&playerValue==="paper"){
-    return 'computer'
-} else return 'player';
-}
 function playerInteract(value){
-    let gameScore = 0;
-    let winner = playRound(value)
-    if (winner==='draw'){
-        return  modifyText("Draw")
-    } else if (winner==='player'){
-        gameScore++
-        return modifyText("Player Wins Round!");
-    } else gameScore--
-    return modifyText("Computer Wins Round!");
-
-    function modifyText(text) {
-        return interact.innerHTML = text;
+    let winner = value;
+    if (winner==='player'){
+        playerScore++;
+        interact.innerHTML=`Player Wins Round! P:${playerScore} C:${computerScore}`
+    } else if (winner==='computer'){
+        computerScore++;
+        interact.innerHTML=`Computer Wins Round! P:${playerScore} C:${computerScore}`
+        return
+    } else {
+        interact.innerHTML=`Draw! P:${playerScore} C:${computerScore}`
     }
+}
+function checkScore(playerScore, computerScore){
+        if (playerScore===5){
+            interact.innerHTML="Player Wins Game, Congrats!!";
+            resetScore();
+            return
+        } else if(computerScore===5){
+            interact.innerHTML="Computer Wins Game, Try Again?";
+            resetScore();
+            return
+        }
+    }
+function playRound(playerValue){
+    let computerValue = computerPlay(getRandomInt(3));
+    if (computerValue===playerValue){
+        return 'draw'
+    }else if (computerValue==='rock'&&playerValue==='scissors'||computerValue==='paper'&&playerValue==='rock'||computerValue==='scissors'&&playerValue==="paper"){
+        return 'computer'
+    } else return 'player';
+}
+function resetScore(){
+    playerScore = 0;
+    computerScore = 0;
 }
